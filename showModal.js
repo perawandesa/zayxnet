e548dc2355720d52f43c80606a73e983221577f4
perawandesa <124488333+perawandesa@users.noreply.github.com>
2025-09-09 15:29:00 +0800

    Update showModal.js

--- showModal.js
+++ showModal.js
@@ -2,8 +2,8 @@
       $('#lihatModal').modal('show');
       var judul = obj.getAttribute('data-judul');
       var jelas = obj.getAttribute('data-jelas');
-      var url = obj.getAttribute('data-url');
-      var href = obj.getAttribute('href');
+      var url = obj.getAttribute('href');
+      var href = obj.getAttribute('data-href');
       var video = obj.getAttribute('data-video');
       $('#modalTitle').empty();
       $('#modalTitle').append(judul);
@@ -14,13 +14,13 @@
       $('#loadingIframe').append('Memuat document...');
       $('#iframeContent').empty();
       
-      $('#iframeContent').append('<iframe id="iframe" class="responsive-iframe" src="https://drive.google.com/file/d/'+href+'/preview" ></iframe>');
+      $('#iframeContent').append('<iframe id="iframe" class="responsive-iframe" src="https://drive.google.com/file/d/'+url+'/preview" ></iframe>');
       $('#iframe').one('load', function(){$('#loadingIframe').empty();});
       
       $('#loadingUnduh').empty();
       $('#loadingUnduh').append('download document...');
       $('#unduhContent').empty();
-      $('#unduhContent').append('<a id="unduhContent" href="https://drive.google.com/uc?export=download&id='+href+'" rel="nofollow">UNDUH</a>');
+      $('#unduhContent').append('<a id="unduhContent" href="https://drive.google.com/uc?export=download&id='+url+'" rel="nofollow">UNDUH</a>');
       $('#unduh').one('load', function(){$('#loadingUnduh').empty();});
   
       $('#loadingTonton').empty();
@@ -29,5 +29,6 @@
       $('#tontonContent').append('<iframe id="tonton" class="responsive-iframe"  src="https://www.youtube.com/embed/'+video+'?autoplay=1&mute=1&controls=0&showinfo=0&autohide=1&version=3&loop=1&playlist='+video+'&enablejsapi=1" ></iframe>');
       $('#tonton').one('load', function(){$('#loadingTonton').empty();});       
     }
+
 
 
